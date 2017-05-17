217fc4f4047b9eacceeae69b9af4d60699257a38
MS Intune <msintuneappsdk@microsoft.com>
2017-05-17 13:43:58 -0400

    v.7.0.1 fixing Headers

---
+++ Headers/IntuneMAMDefs.h
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//
+
+// Result codes for switching identities.
+typedef NS_ENUM(NSUInteger, IntuneMAMSwitchIdentityResult)
+{
+    IntuneMAMSwitchIdentityResultSuccess,
+    IntuneMAMSwitchIdentityResultCanceled,
+    IntuneMAMSwitchIdentityResultNotAllowed,
+    IntuneMAMSwitchIdentityResultFailed
+};
